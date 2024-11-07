48bad8bea775770dea157fda1e113a67b9798318
Brandon-Bonilla <branbonilla@gmail.com>
2024-11-06 19:35:08 -0600

    Proyecto Final

---
+++ src/main/java/com/mycompany/proyectolenguajes/ProyectoLenguajes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectolenguajes;
+
+/**
+ *
+ * @author Brandon
+ */
+public class ProyectoLenguajes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/proyectolenguajes/conexiones.java
@@ -0,0 +1,41 @@
+
+package com.mycompany.proyectolenguajes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class conexiones {
+    private final String DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private final String URL = "jdbc:oracle:thin:@//localhost:1521/orcl";
+    private final String USER = "UsuarioPruebas";
+    private final String PASWORD = "Usuario1";
+
+    public Connection cadena;
+
+    public conexiones() {
+         this.cadena = null;
+    }
+
+    public Connection conectar() {
+        try {
+            Class.forName(DRIVER);
+            this.cadena = DriverManager.getConnection(URL, USER, PASWORD);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            System.exit(0);
+        }
+        return this.cadena;
+
+    }
+
+    public void desconectar() {
+        try {
+            this.cadena.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+}
